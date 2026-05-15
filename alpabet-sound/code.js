49d752c0b6ca0933306d4b3f87fd9a91f9297871
batoolw872-stack <batoolw872@gmail.com>
2026-05-16 00:15:15 +0500

    Add files via upload

---
+++ alpabet-sound/code.js
@@ -0,0 +1,34 @@
+
+
+var shah=document.getElementById('hey').onclick=abc;
+var shah=document.getElementById('hey').ondblclick=abcd;
+var shah=document.getElementById('hey').oncontextmenu=abcd1;
+// var shah=document.getElementById('hey').innerText="ahmed";
+var shah=document.getElementById('hey').onmouseenter=abcd2;
+var shah=document.getElementById('hey').onmouseover=abcdh;
+
+
+
+ function abcdh(){
+    document.getElementById("hey").style.backgroundColor="yellow";
+
+ }
+
+function abc(){
+    document.getElementById("hey").style.backgroundColor="red";
+}
+
+function abcd(){
+    document.getElementById("hey").style.backgroundColor="";
+}
+
+function abcd1(){
+    document.getElementById("hey").style.backgroundColor="green";
+}
+
+// function abcd2(){
+//     document.getElementById("hey").style.Color="purple";
+// }
+
+
+
